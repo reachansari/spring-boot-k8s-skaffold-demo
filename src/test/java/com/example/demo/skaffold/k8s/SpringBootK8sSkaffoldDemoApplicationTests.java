e1fad08acbe9ba887e491e1c5d1c4c309c3c146a
Thameem <reachansari@gmail.com>
2021-10-02 16:24:32 +0800

    first commit

---
+++ src/test/java/com/example/demo/skaffold/k8s/SpringBootK8sSkaffoldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.skaffold.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sSkaffoldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
